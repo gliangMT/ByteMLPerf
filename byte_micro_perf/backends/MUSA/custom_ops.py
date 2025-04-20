f9f3762aa2eed0d031466f17ec4484375218883b
gliangMT <liang.geng@mthreads.com>
2025-04-20 11:47:28 +0800

    add f8 group gemm

--- byte_micro_perf/backends/MUSA/custom_ops.py
+++ byte_micro_perf/backends/MUSA/custom_ops.py
@@ -10,7 +10,7 @@ from typing import List, Dict, Union, Tuple
 FILE_DIR = pathlib.Path(__file__).parent.absolute()
 MICRO_PERF_DIR = FILE_DIR.parent.parent
 
-ITERS = 10
+ITERS = 1000
 
 sys.path.insert(0, str(MICRO_PERF_DIR))
 
@@ -20,6 +20,14 @@ from core.op import BasicOp
 from core.ops.gemm_ops import GemmOp, GemmFP8Op, GroupGemmFP8Op
 from core.ops.attn_ops import FlashAttentionOp
 
+# group fp8 gemm
+from transformer_engine.pytorch.cpp_extensions import general_gemm, general_grouped_gemm
+from transformer_engine.pytorch.tensor.float8_tensor import Float8Quantizer
+from transformer_engine.pytorch.module.base import (
+    get_multi_stream_cublas_workspace,
+    get_workspace,
+)
+import transformer_engine_torch as tex
 
 """
 gemm ops
@@ -36,138 +44,16 @@ class GPUGemmOp(GemmOp):
             torch.backends.mudnn.allow_tf32 = True
 
 
-# try:
-#     import deep_gemm
-#     from deep_gemm import (
-#         bench_kineto,
-#         calc_diff,
-#         ceil_div,
-#         get_col_major_tma_aligned_tensor,
-#     )
-# except ImportError:
-#     deep_gemm = None
-# logger.warning("deep_gemm is not available, please install it first.")
-
 FP8_E4M3_MAX = 448.0  # Maximum representable value in FP8 E4M3 format
-
-
-# def per_token_cast_to_fp8(
-#     x: torch.Tensor, group_size=128
-# ) -> Tuple[torch.Tensor, torch.Tensor]:
-#     assert x.dim() == 2 and x.size(1) % group_size == 0
-#     m, n = x.shape
-#     x_view = x.view(m, -1, group_size)
-#     x_amax = x_view.abs().float().amax(dim=2).view(m, -1).clamp(1e-4)
-#     return (
-#         (x_view * (FP8_E4M3_MAX / x_amax.unsqueeze(2)))
-#         .to(torch.float8_e4m3fn)
-#         .view(m, n),
-#         (x_amax / FP8_E4M3_MAX).view(m, -1),
-#     )
-
-
-# def per_block_cast_to_fp8(
-#     x: torch.Tensor, group_size=128
-# ) -> Tuple[torch.Tensor, torch.Tensor]:
-#     assert x.dim() == 2
-#     m, n = x.shape
-#     x_padded = torch.zeros(
-#         (ceil_div(m, group_size) * group_size, ceil_div(n, group_size) * group_size),
-#         dtype=x.dtype,
-#         device=x.device,
-#     )
-#     x_padded[:m, :n] = x
-#     x_view = x_padded.view(-1, group_size, x_padded.size(1) // group_size, group_size)
-#     x_amax = x_view.abs().float().amax(dim=(1, 3), keepdim=True).clamp(1e-4)
-#     x_scaled = (x_view * (FP8_E4M3_MAX / x_amax)).to(torch.float8_e4m3fn)
-#     return x_scaled.view_as(x_padded)[:m, :n].contiguous(), (
-#         x_amax / FP8_E4M3_MAX
-#     ).view(x_view.size(0), x_view.size(2))
-
-
-# def construct(m: int, k: int, n: int, group_size, device) -> Tuple[
-#     Tuple[torch.Tensor, torch.Tensor],
-#     Tuple[torch.Tensor, torch.Tensor],
-#     torch.Tensor,
-#     torch.Tensor,
-# ]:
-#     x = torch.randn((m, k), device=device, dtype=torch.bfloat16)
-#     y = torch.randn((n, k), device=device, dtype=torch.bfloat16)
-#     out = torch.empty((m, n), device=device, dtype=torch.bfloat16)
-
-#     x_fp8, y_fp8 = per_token_cast_to_fp8(x, group_size), per_block_cast_to_fp8(
-#         y, group_size
-#     )
-#     # Transpose earlier so that the testing will not trigger transposing kernels
-#     x_fp8 = (x_fp8[0], deep_gemm.get_col_major_tma_aligned_tensor(x_fp8[1]))
-#     return x_fp8, y_fp8, out
-
-
-# def construct_grouped(
-#     num_groups: int, m: int, k: int, n: int, is_masked: bool, group_size, device
-# ) -> Tuple[
-#     Tuple[torch.Tensor, torch.Tensor],
-#     Tuple[torch.Tensor, torch.Tensor],
-#     torch.Tensor,
-#     torch.Tensor,
-# ]:
-#     x = torch.randn((num_groups, m, k), device=device, dtype=torch.bfloat16)
-#     y = torch.randn((num_groups, n, k), device=device, dtype=torch.bfloat16)
-#     out = torch.empty((num_groups, m, n), device=device, dtype=torch.bfloat16)
-
-#     assert m % 4 == 0, f"TMA alignment error: {m}"
-#     x_fp8 = (
-#         torch.empty_like(x, dtype=torch.float8_e4m3fn),
-#         torch.empty((num_groups, m, k // group_size), device=device, dtype=torch.float),
-#     )
-#     y_fp8 = (
-#         torch.empty_like(y, dtype=torch.float8_e4m3fn),
-#         torch.empty(
-#             (num_groups, (n + group_size - 1) // group_size, k // group_size),
-#             device=device,
-#             dtype=torch.float,
-#         ),
-#     )
-#     for i in range(num_groups):
-#         x_fp8[0][i], x_fp8[1][i] = per_token_cast_to_fp8(x[i], group_size)
-#         y_fp8[0][i], y_fp8[1][i] = per_block_cast_to_fp8(y[i], group_size)
-
-#     # For non-masked input, we must merge the group and M dims
-#     if not is_masked:
-#         x_fp8 = (
-#             x_fp8[0].view(-1, k),
-#             per_token_cast_to_fp8(x.view(-1, k), group_size)[1],
-#         )
-#         out = out.view(-1, n)
-
-#     # Transpose earlier so that the testing will not trigger transposing kernels
-#     x_fp8 = (x_fp8[0], get_col_major_tma_aligned_tensor(x_fp8[1]))
-#     return x_fp8, y_fp8, out
 
 
 class GPUGemmFP8Op(GemmFP8Op):
     def __init__(self, args_dict, backend, *args, **kwargs):
-        # if deep_gemm is None:
-        #     raise ImportError("deep_gemm is not available, please install it first.")
 
         super().__init__(args_dict, backend, *args, **kwargs)
 
         self._custom_run = True
         self._run_func = self.gemm_fp8_run
-
-    # def gemm_fp8_run(self):
-    #     def test_func():
-    #         x_fp8, y_fp8, out = construct(
-    #             self.M,
-    #             self.K,
-    #             self.N,
-    #             self.quant_group_size,
-    #             self.backend.get_torch_device_name(),
-    #         )
-    #         deep_gemm.gemm_fp8_fp8_bf16_nt(x_fp8, y_fp8, out)
-
-    #     t = bench_kineto(test_func, "fp8_gemm", suppress_kineto_output=True)
-    #     return t * 1e6 # us
 
     def test_scaled_mm_with_cast(self):
         """
@@ -234,61 +120,91 @@ class GPUGemmFP8Op(GemmFP8Op):
 
 class GPUGroupGemmFP8Op(GroupGemmFP8Op):
     def __init__(self, args_dict, backend, *args, **kwargs):
-        if deep_gemm is None:
-            raise ImportError("deep_gemm is not available, please install it first.")
-
         super().__init__(args_dict, backend, *args, **kwargs)
 
         self._custom_run = True
         self._run_func = self.group_gemm_fp8_run
 
+    def test_fp8_grouped_gemm(self, accumulate=True):
+        device = self.backend.get_torch_device_name()
+        # z, m, k, n = shape
+        z = self.num_groups
+        m = self.M
+        k = self.K
+        n = self.N
+        m_splits = m // z
+
+        dtype = torch.bfloat16
+        A = [torch.randn(n, k, dtype=dtype, device=device) for _ in range(z)]  # weight
+        B = torch.split(
+            torch.randn(m, k, dtype=dtype, device=device), m_splits
+        )  # input
+        out = torch.split(
+            torch.randn(m, n, dtype=dtype, device=device), m_splits
+        )  # output
+        out_ref = [o.clone() for o in out]
+
+        # fp8 should be robust enough to this fake scale
+        scale = 1 + torch.rand(1, dtype=torch.float32, device=device).squeeze()
+        amax = torch.zeros(1, 1, dtype=torch.float32, device=device)
+
+        a_quantizers = [
+            Float8Quantizer(
+                scale.clone(),
+                amax.clone(),
+                tex.DType.kFloat8E4M3,
+            )
+            for _ in range(z)
+        ]
+        b_quantizers = [
+            Float8Quantizer(
+                scale.clone(),
+                amax.clone(),
+                tex.DType.kFloat8E4M3,
+            )
+            for _ in range(z)
+        ]
+
+        A_fp8 = []
+        B_fp8 = []
+
+        for i in range(z):
+            A_fp8.append(a_quantizers[i](A[i]))
+            B_fp8.append(b_quantizers[i](B[i]))
+
+        # warm up
+        general_grouped_gemm(
+            A_fp8,
+            B_fp8,
+            out,
+            dtype,
+            get_multi_stream_cublas_workspace(),
+            m_splits=[k] * m_splits,
+            accumulate=accumulate,
+        )
+
+        start_time = time.perf_counter()
+        for _ in range(ITERS):
+            general_grouped_gemm(
+                A_fp8,
+                B_fp8,
+                out,
+                dtype,
+                get_multi_stream_cublas_workspace(),
+                m_splits=[k] * m_splits,
+                accumulate=accumulate,
+            )
+        end_time = time.perf_counter()
+
+        exec_time = end_time - start_time
+
+        tflops = 2 * m * k * n * z / (exec_time * 1e12)
+        print(f">>>>>>>>>> actual calc flops is {tflops} ")
+
+        return (exec_time / ITERS) * 1e6  # us
+
     def group_gemm_fp8_run(self):
-
-        def test_func_contiguous():
-            x_fp8, y_fp8, out = construct_grouped(
-                self.num_groups,
-                self.M,
-                self.K,
-                self.N,
-                False,
-                self.quant_group_size,
-                self.backend.get_torch_device_name(),
-            )
-            m_indices = torch.arange(0, self.num_groups, device="musa", dtype=torch.int)
-            m_indices = (
-                m_indices.unsqueeze(-1)
-                .expand(self.num_groups, self.M)
-                .contiguous()
-                .view(-1)
-            )
-            deep_gemm.m_grouped_gemm_fp8_fp8_bf16_nt_contiguous(
-                x_fp8, y_fp8, out, m_indices
-            )
-
-        def test_func_masked():
-            x_fp8, y_fp8, out = construct_grouped(
-                self.num_groups,
-                self.M,
-                self.K,
-                self.N,
-                True,
-                self.quant_group_size,
-                self.backend.get_torch_device_name(),
-            )
-            masked_m = (
-                torch.ones((self.num_groups,), device="musa", dtype=torch.int) * self.M
-            )
-            deep_gemm.m_grouped_gemm_fp8_fp8_bf16_nt_masked(
-                x_fp8, y_fp8, out, masked_m, self.M
-            )
-
-        if self.mode == "contiguous":
-            t = bench_kineto(
-                test_func_contiguous, "fp8_gemm", suppress_kineto_output=True
-            )
-        elif self.mode == "masked":
-            t = bench_kineto(test_func_masked, "fp8_gemm", suppress_kineto_output=True)
-        return t * 1e6
+        return self.test_fp8_grouped_gemm()
 
 
 """
